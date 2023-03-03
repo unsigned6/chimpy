f1d7b93e8a664f0a0f5ddaa01a189e5b2a808949
Timur Bazhirov <timur@exabyte.io>
2023-03-03 05:24:29 -0800

    fix: path

--- scripts/link-bin.sh
+++ scripts/link-bin.sh
@@ -3,9 +3,7 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Set initial dir to avoid differences between OS
-cd $SCRIPT_DIR
-
-cd  ../node_modules/.bin/
+cd $SCRIPT_DIR/../../.bin/
 ln -sf ../chimpy/bin/chimp.js chimp
 ln -sf ../chimpy/bin/chimpy.js chimpy
 cd -
