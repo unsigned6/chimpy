6e33df8adabc0020e9c0fb4eb8c312e3136b59c7
Timur Bazhirov <timur@exabyte.io>
2023-06-19 22:50:43 -0700

    chore: update linking script

--- scripts/link-bin.sh
+++ scripts/link-bin.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+echo "SCRIPT_DIR:" $SCRIPT_DIR
 # Set initial dir to avoid differences between OS
-cd $SCRIPT_DIR/../../.bin/
+cd $SCRIPT_DIR/../node_modules/.bin/
 ln -sf ../chimpy/bin/chimp.js chimp
 ln -sf ../chimpy/bin/chimpy.js chimpy
 cd -
