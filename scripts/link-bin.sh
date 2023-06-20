3a95b933ec151b470326ab1af722d167c83ee1d3
Timur Bazhirov <timur@exabyte.io>
2023-06-19 23:03:07 -0700

    chore: update linking logic to include direct and install as a dependency

--- scripts/link-bin.sh
+++ scripts/link-bin.sh
@@ -2,8 +2,13 @@
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "SCRIPT_DIR:" $SCRIPT_DIR
-# Set initial dir to avoid differences between OS
-cd $SCRIPT_DIR/../node_modules/.bin/
+# Set initial dir to avoid differences between OS and whether package is installed as dependency
+# If as dependency: <ROOT>/node_modules/chimpy/scripts/../../node_modules/.bin/
+if [ -d "$SCRIPT_DIR/../node_modules/.bin/" ]; then
+  cd $SCRIPT_DIR/../node_modules/.bin/
+else
+  cd $SCRIPT_DIR/../../../node_modules/.bin/
+fi
 ln -sf ../chimpy/bin/chimp.js chimp
 ln -sf ../chimpy/bin/chimpy.js chimpy
 cd -
